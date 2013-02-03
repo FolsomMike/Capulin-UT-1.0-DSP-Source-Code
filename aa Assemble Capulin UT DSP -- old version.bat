276b25cf24d5e89ff5b4a90f36bc70dbf0d9189f
Mike Schoonover <mike@folsomvillage.com>
2013-02-02 22:01:20 -0700

    Old version of batch file for assembly deleted.
    Signed-off-by: Mike Schoonover <mike@folsomvillage.com>

--- aa Assemble Capulin UT DSP -- old version.bat
+++
@@ -1,5 +0,0 @@
-"..\..\..\TMS320C54x Tools\asm500.exe" "Capulin UT DSP.asm" -l
-"..\..\..\TMS320C54x Tools\asm500.exe" "Capulin UT DSP Debug.asm" -l
-"..\..\..\TMS320C54x Tools\lnk500.exe" "Capulin UT DSP.cmd"
-"..\..\..\TMS320C54x Tools\hex500.exe" -a "Capulin UT DSP.out" -o "CapulinUTDSP.hex" -romwidth 16
-pause
