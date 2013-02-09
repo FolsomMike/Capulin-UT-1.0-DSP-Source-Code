203c8e51754bf369e6c2c06f13b5cf033ec52ded
Mike Schoonover <mike@folsomvillage.com>
2013-02-09 12:03:06 -0700

    Added code to batch file to create an absolute address .abs file which is useful for debugging or as an input to a simulation program.
    Fixed bug where hex500 needs a output filename with no spaces.
    
    Signed-off-by: Mike Schoonover <mike@folsomvillage.com>

--- aa Assemble Capulin UT DSP.bat
+++ aa Assemble Capulin UT DSP.bat
@@ -1,1 +1,4 @@
-"aa Assemble Program.bat" "Capulin UT DSP"
+"aa Assemble Program.bat" "Capulin UT DSP" "CapulinUTDSP"
+
+@REM note that the hex500 program called by the "aa Assemble Program.bat" file cannot output
+@REM to a filename with spaces, so the second parameter is used to provide a usable name
