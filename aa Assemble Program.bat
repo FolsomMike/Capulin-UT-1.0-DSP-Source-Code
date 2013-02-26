340d86ef6a7ae9f2ab77829175fce941134d2e01
Mike Schoonover <mike@folsomvillage.com>
2013-02-26 12:19:43 -0700

    Fixed "aa Assemble Program.bat" to work with the Universal IDE.
    Updated .gitignore to ignore the file listing results of the compile.
    
    Signed-off-by: Mike Schoonover <mike@folsomvillage.com>

--- aa Assemble Program.bat
+++ aa Assemble Program.bat
@@ -1,13 +1,13 @@
-"..\..\..\TMS320C54x Tools\asm500.exe" "%~1.asm" -l
+"..\..\..\TMS320C54x Tools\asm500.exe" "%~1.asm" -l 1>>results.txt 2>&1
 
-"..\..\..\TMS320C54x Tools\lnk500.exe" "%~1.cmd"
+"..\..\..\TMS320C54x Tools\lnk500.exe" "%~1.cmd" 1>>results.txt 2>&1
 
 @REM note that hex500 cannot output to a filename with spaces, so %2 is used to provide a usable name
 
-"..\..\..\TMS320C54x Tools\hex500.exe" -a "%~1.out" -o "%~2.hex" -romwidth 16
+"..\..\..\TMS320C54x Tools\hex500.exe" -a "%~1.out" -o "%~2.hex" -romwidth 16  1>>results.txt 2>&1
 
-"..\..\..\TMS320C54x Tools\abs500.exe" "%~1.out"
+"..\..\..\TMS320C54x Tools\abs500.exe" "%~1.out" 1>>results.txt 2>&1
 
-"..\..\..\TMS320C54x Tools\asm500.exe" -a "%~1.abs"
+"..\..\..\TMS320C54x Tools\asm500.exe" -a "%~1.abs" 1>>results.txt 2>&1
 
-pause
+@rem pause
