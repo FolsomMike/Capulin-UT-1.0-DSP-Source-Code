146b0f43160db2c45f1e7a492330208d0f9e7a69
Mike Schoonover <mike@folsomvillage.com>
2013-08-07 17:08:31 -0600

    Added entries to compile and link the companion "Debug" assembler file. That file was dropped when the old Code Composer was obsoleted, but now it is used again for general purpose debugging and eventually will be used with on-board debugger.
    Only added '"Capulin UT DSP Debug.obj"' line to the .cmd file, but Git
    shows the entire code was replaced for some reason.
    
    Signed-off-by: Mike Schoonover <mike@folsomvillage.com>

--- aa Assemble Program.bat
+++ aa Assemble Program.bat
@@ -1,10 +1,16 @@
 "..\..\..\TMS320C54x Tools\asm500.exe" "%~1.asm" -l 1>>results.txt 2>&1
 
+"..\..\..\TMS320C54x Tools\asm500.exe" "%~1 Debug.asm" -l 1>>results.txt 2>&1
+
 "..\..\..\TMS320C54x Tools\lnk500.exe" "%~1.cmd" 1>>results.txt 2>&1
+
 
 @REM note that hex500 cannot output to a filename with spaces, so %2 is used to provide a usable name
 
 "..\..\..\TMS320C54x Tools\hex500.exe" -a "%~1.out" -o "%~2.hex" -romwidth 16  1>>results.txt 2>&1
+
+
+@REM Run the absolute address generator on the file to produce a listing with absolute addresses
 
 "..\..\..\TMS320C54x Tools\abs500.exe" "%~1.out" 1>>results.txt 2>&1
 
